71f8bce3fe696725f184d9075abad2cbee14e789
abhishek nag <abhishek123nag@gmail.com>
2017-06-08 08:28:38 +0530

    After III
    Added Game State Manager classs to manage game screens
    MenuState CLass for managing game menu screen

--- core/src/com/abhnin/flappy/FlappyBird.java
+++ core/src/com/abhnin/flappy/FlappyBird.java
@@ -1,5 +1,7 @@
 package com.abhnin.flappy;
 
+import com.abhnin.flappy.states.GameStateManager;
+import com.abhnin.flappy.states.MenuState;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -7,31 +9,31 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	public static final int WIDTH = 400;
 	public static final int HEIGHT = 600;
 	public static final String TITLE = "Flappy Bird";
+
+	private GameStateManager gsm;
+    private SpriteBatch batch;
 
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        gsm = new GameStateManager();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/abhnin/flappy/states/MenuState.java
@@ -0,0 +1,30 @@
+package com.abhnin.flappy.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Sony on 6/8/2017.
+ */
+
+public class MenuState extends State {
+
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
