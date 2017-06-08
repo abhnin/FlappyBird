a91715a044fbb0a637ef34b15bbbccbe21940f39
abhishek nag <abhishek123nag@gmail.com>
2017-06-08 10:59:52 +0530

    After IV
    Menu Screen

--- core/src/com/abhnin/flappy/states/MenuState.java
+++ core/src/com/abhnin/flappy/states/MenuState.java
@@ -1,5 +1,7 @@
 package com.abhnin.flappy.states;
 
+import com.abhnin.flappy.FlappyBird;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
@@ -7,10 +9,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 
 public class MenuState extends State {
-
+    private Texture background;
+    private Texture playBtn;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
     }
 
     @Override
@@ -25,6 +30,11 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playBtn, (FlappyBird.WIDTH / 2 ) - (playBtn.getWidth() / 2 ), (FlappyBird.HEIGHT / 2 ) - (playBtn.getHeight() / 2 ) );
+        sb.end();
+
 
     }
 }
